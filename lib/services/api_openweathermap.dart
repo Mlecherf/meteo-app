7bbdb29c7a5db631a47fa281da8af47af69e8e3e
MrTanguy <tanguy.meignier@gmail.com>
2022-06-07 10:15:09 +0200

    update API

--- lib/services/api_openweathermap.dart
+++ lib/services/api_openweathermap.dart
@@ -5,6 +5,8 @@ import '../models/openweathermap.dart';
 import 'package:http/http.dart' as http;
 
 Future<Openweathermap> getDataAPI(String city) async {
+  Openweathermap dataCity;
+
   final queryParameters = {
     'q': city,
     'appid': '3788670491baf2cb8bd1d0e95e6119e8',
@@ -18,9 +20,12 @@ Future<Openweathermap> getDataAPI(String city) async {
 
   if (response.statusCode == 200) {
     var jsonReponse = jsonDecode(response.body);
+    dataCity = Openweathermap(jsonReponse['weather'], jsonReponse['main']);
 
     /// BLABLABLA
   } else {
-    print('Request failed with status: ${response.statusCode}.');
+    //print('Request failed with status: ${response.statusCode}.');
+    return Future.error('error');
   }
+  return (dataCity);
 }

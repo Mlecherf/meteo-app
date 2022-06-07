3a2c230f70ee2aa786add8f02d40ad90dab669f9
Mlecherf <mathis.lecherfpro@gmail.com>
2022-06-07 11:36:17 +0200

    Added UI

--- lib/main.dart
+++ lib/main.dart
@@ -29,22 +29,194 @@ class MyHomePage extends StatefulWidget {
 }
 
 class _MyHomePageState extends State<MyHomePage> {
+  final GlobalKey<ScaffoldState> _scaffoldState = GlobalKey<ScaffoldState>();
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      appBar: AppBar(
-        title: Text(widget.title),
-      ),
-      body: Center(
-        child: Column(
-          mainAxisAlignment: MainAxisAlignment.center,
-          children: const <Widget>[
-            Text(
-              'You have pushed the button this many times:',
-            ),
-          ],
+        key: _scaffoldState,
+        drawer: Drawer(
+          backgroundColor: const Color.fromRGBO(40, 45, 53, 1),
+          child: ListView(
+            padding: EdgeInsets.zero,
+            children: [
+              Container(
+                decoration: const BoxDecoration(
+                  border: Border(
+                      bottom: BorderSide(
+                          color: Color.fromARGB(255, 135, 135, 135))),
+                ),
+                child: Column(children: <Widget>[
+                  const SizedBox(height: 100),
+                  const Text('My Cities',
+                      style: TextStyle(
+                          color: Colors.white, fontStyle: FontStyle.normal)),
+                  const SizedBox(height: 30),
+                  ElevatedButton(
+                    onPressed: () {
+                      showDialog(
+                          context: context,
+                          builder: (context) {
+                            return Dialog(
+                                child: Container(
+                              height: 150.0,
+                              width: 360.0,
+                              child: ListView(
+                                children: <Widget>[
+                                  SizedBox(height: 20),
+                                  const Center(
+                                    child: Text(
+                                      "Add a city",
+                                      style: TextStyle(
+                                        fontSize: 24,
+                                        color: Colors.blue,
+                                        fontWeight: FontWeight.bold,
+                                        fontStyle: FontStyle.italic,
+                                      ),
+                                    ),
+                                  ),
+                                  SizedBox(height: 20),
+                                  TextField(
+                                    onSubmitted: (String value) async {
+                                      Navigator.pop(context);
+                                    },
+                                  ),
+                                ],
+                              ),
+                            ));
+                          });
+                    },
+                    child: const Text('Add a city'),
+                  ),
+                  const SizedBox(height: 30),
+                ]),
+              ),
+              ListTile(
+                hoverColor: const Color.fromARGB(255, 135, 135, 136),
+                textColor: const Color.fromARGB(255, 255, 255, 255),
+                title: Text.rich(
+                  TextSpan(
+                    style: const TextStyle(
+                      fontSize: 17,
+                    ),
+                    children: [
+                      const WidgetSpan(
+                          child: SizedBox(
+                        width: 110,
+                      )),
+                      const TextSpan(
+                        text: 'Paris',
+                      ),
+                      const WidgetSpan(
+                          child: SizedBox(
+                        width: 70,
+                      )),
+                      WidgetSpan(
+                        child: IconButton(
+                          icon: const Icon(Icons.delete,
+                              color: Color.fromARGB(255, 246, 246, 246)),
+                          onPressed: () {
+                            Navigator.pop(context);
+                          },
+                        ),
+                      ),
+                    ],
+                  ),
+                ),
+                onTap: () {
+                  Navigator.pop(context);
+                },
+              ),
+            ],
+          ),
         ),
-      ),
-    );
+        body: Center(
+          child: Stack(children: <Widget>[
+            Container(
+              decoration: const BoxDecoration(
+                image: DecorationImage(
+                  image: AssetImage("assets/chilly_night.jpg"),
+                  fit: BoxFit.fitWidth,
+                ),
+              ),
+            ),
+            Container(
+                child: Row(children: <Widget>[
+              const Padding(
+                padding: EdgeInsets.fromLTRB(10, 80, 0, 0),
+              ),
+              IconButton(
+                icon: const Icon(Icons.menu, color: Colors.white, size: 40),
+                onPressed: () {
+                  _scaffoldState.currentState!.openDrawer();
+                },
+              )
+            ])),
+            Container(
+              alignment: Alignment.topCenter,
+              child: Row(children: <Widget>[
+                Padding(
+                  padding: EdgeInsets.fromLTRB(
+                      ((MediaQuery.of(context).size.width) / 2) - 40,
+                      (MediaQuery.of(context).size.height) / 3,
+                      0,
+                      0),
+                ),
+                const Text(
+                  'Paris',
+                  style: TextStyle(
+                      color: Colors.white,
+                      fontWeight: FontWeight.bold,
+                      fontSize: 35.0),
+                ),
+              ]),
+            ),
+            Container(
+              alignment: Alignment.topCenter,
+              child: Row(children: <Widget>[
+                Padding(
+                  padding: EdgeInsets.fromLTRB(
+                      ((MediaQuery.of(context).size.width) / 2 - 15),
+                      (MediaQuery.of(context).size.height) - 110,
+                      0,
+                      0),
+                ),
+                const Text(
+                  '9 °',
+                  style: TextStyle(color: Colors.white, fontSize: 70.0),
+                ),
+              ]),
+            ),
+            Container(
+              margin: const EdgeInsets.only(top: 400.0, bottom: 136),
+              padding: const EdgeInsets.all(3.0),
+              decoration: const BoxDecoration(
+                  color: Color.fromARGB(126, 46, 46, 46),
+                  border: Border(
+                      top: BorderSide(color: Color.fromARGB(255, 0, 0, 0)),
+                      bottom: BorderSide(color: Color.fromARGB(255, 0, 0, 0)))),
+              child: Row(children: <Widget>[
+                const SizedBox(width: 10),
+                Column(children: const <Widget>[
+                  Text(
+                    'Tuesday',
+                    style: TextStyle(color: Colors.white, fontSize: 25.0),
+                  ),
+                ]),
+                const Spacer(),
+                Column(children: const <Widget>[
+                  Text(
+                    '9°',
+                    style: TextStyle(color: Colors.white, fontSize: 25.0),
+                  ),
+                  Text(
+                    "Cloudy",
+                    style: TextStyle(color: Colors.white, fontSize: 15.0),
+                  )
+                ]),
+                const SizedBox(width: 10),
+              ]),
+            ),
+          ]),
+        ));
   }
 }

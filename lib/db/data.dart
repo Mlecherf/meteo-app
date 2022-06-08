d1f6f3e6fec89697a76a00c7188ab743f8df2bd2
Mlecherf <mathis.lecherfpro@gmail.com>
2022-06-08 10:28:04 +0200

    Added DB

--- lib/db/data.dart
+++ lib/db/data.dart
@@ -1,7 +1,10 @@
 import 'package:sqflite/sqflite.dart';
+// ignore: depend_on_referenced_packages
+import 'package:path/path.dart';
+import 'package:meteo_app/models/openweathermap.dart';
 
 class Data {
-  static final Data data = Data._init();
+  static final Data instance = Data._init();
 
   static Database? _database;
 
@@ -10,7 +13,7 @@ class Data {
   Future<Database> get database async {
     if (_database != null) return _database!;
 
-    _database = await _initDB('./data.db');
+    _database = await _initDB('cities.db');
     return _database!;
   }
 
@@ -22,11 +25,72 @@ class Data {
   }
 
   Future _createDB(Database db, int version) async {
-    await db.execute('CREATE TABLE ');
+    const idType = 'INTEGER PRIMARY KEY AUTOINCREMENT';
+
+    const textType = 'TEXT NOT NULL';
+
+    await db.execute('''
+    CREATE TABLE $tableCities (
+      ${CityFields.id} $idType,
+      ${CityFields.name} $textType
+    )
+    ''');
+  }
+
+  Future create(City city) async {
+    final db = await instance.database;
+    final id = await db.insert(tableCities, city.toJson());
+    return city.copy(id: id);
+  }
+
+  Future<City> readCity(int id) async {
+    final db = await instance.database;
+
+    final maps = await db.query(
+      tableCities,
+      columns: CityFields.values,
+      where: '${CityFields.id} = ?',
+      whereArgs: [id],
+    );
+
+    if (maps.isNotEmpty) {
+      return City.fromJson(maps.first);
+    } else {
+      throw Exception('ID $id not found');
+    }
+  }
+
+  Future<List<City>> readAllCities() async {
+    final db = await instance.database;
+
+    final result = await db.query(tableCities);
+
+    return result.map((json) => City.fromJson(json)).toList();
+  }
+
+  Future<int> update(City city) async {
+    final db = await instance.database;
+
+    return db.update(
+      tableCities,
+      city.toJson(),
+      where: '${CityFields.id} = ?',
+      whereArgs: [city.id],
+    );
+  }
+
+  Future<int> delete(int id) async {
+    final db = await instance.database;
+
+    return await db.delete(
+      tableCities,
+      where: '${CityFields.id} = ?',
+      whereArgs: [id],
+    );
   }
 
   Future close() async {
-    final db = await data.database;
+    final db = await instance.database;
 
     db.close();
   }
--- lib/models/openweathermap.dart
+++ lib/models/openweathermap.dart
@@ -16,3 +16,36 @@ class ChildMain {
 
   ChildMain(this.temp);
 }
+
+const String tableCities = 'cities';
+
+class CityFields {
+  static const String id = '_id';
+  static const String name = '_name';
+
+  static final List<String> values = [id, name];
+}
+
+class City {
+  final int? id;
+  final String name;
+
+  const City({this.id, required this.name});
+
+  Map<String, Object?> toJson() => {
+        CityFields.id: id,
+        CityFields.name: name,
+      };
+
+  City copy({
+    int? id,
+    String? name,
+  }) =>
+      City(
+        id: id ?? this.id,
+        name: name ?? this.name,
+      );
+
+  static City fromJson(Map<String, Object?> json) => City(
+      id: json[CityFields.id] as int?, name: json[CityFields.name] as String);
+}
